c95833d82e6a37617fe4b39293b255bd8c4ba58e
tonmoysagar <tonmoysagar@gmail.com>
2016-12-22 10:07:30 +0530

    First Commit

---
+++ app/app.component.ts
@@ -0,0 +1,150 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: `<br>
+             <br>
+			 <br>
+             <img  id ="designerDashboard" src="img/designerDashboard.jpg" alt="Smiley face" height="425px" width="1366px">
+             <br>
+             <br>			 
+			 <u  id="in"><h1 id="intro"> &nbsp;BE A PART OF GHAR BANAO&nbsp; </h1></u>
+			 <p id="opening">
+			   Ghar banao aims to revolutionize the Indian interior design industry through a medley of  cutting edge innovation and exemplary interior design language. And we firmly believe that a great design has the potential to transform the ordinary into brilliance.
+And so we are on a constant lookout for dynamic and talented professionals who can align themselves to our ideals and be a part of Team Ghar Banao. 
+			 </p>
+			 <br>
+			 <br>
+			 <br>
+			 
+			 <div id="outerpoints" style="box-shadow:2px 2px 6px #aaaaaa;">
+			  <br>
+			   <br>
+			 <h1 id="pointheader">PERKS OF JOINING US</h1>
+			 <br>
+			
+			 <br>
+			   <div id="left" style="float: left">
+			      <div id ="point1"  style="height:30px ;width:420px;padding:20px; box-shadow: 2px 2px 6px #aaaaaa;margin-left:200px;border-radius:20px;background-color:#FFFFFF">
+				   
+				    <img src="/img/1.png" style="float:left ;height:30px;width:30px;padding-right:10px;padding-top:3px;border-radius:50%;">
+					<p style="text-align:left;font-weight: lighter;font-family:points;">
+					    Expand your business&#39;s reach to find potential customers throughout India.
+					 </p>
+				  </div>
+				  <br>
+				  
+				  <div id ="points2" style="height:30px ;width:420px;padding:20px; box-shadow: 2px 2px 6px #aaaaaa;margin-left:200px;border-radius:20px;background-color:#FFFFFF">
+				      <img src="/img/2.png" style="float:left ;height:30px;width:30px;padding-right:10px;padding-top:3px;border-radius:50%;"> 
+				     <p style="text-align:left;font-weight: lighter;font-family:points;">
+					   Work anytime,from anywhere.
+					 </p>
+				  </div>
+				  <br>
+				  
+				  <div id="points3" style="height:30px ;width:420px;padding:20px; box-shadow: 2px 2px 6px #aaaaaa;margin-left:200px;border-radius:20px;background-color:#FFFFFF">
+				       <img src="/img/3.png" style="float:left ;height:30px;width:30px;padding-right:10px;padding-top:3px;border-radius:50%;"> 
+				      <p style="text-align:left;font-weight: lighter;font-family:points;">
+				       Build your business by using your own branding and  company name.
+					   <p>
+				  </div>
+				  <br>
+				  
+				  <div  id="points4" style="height:30px ;width:420px;padding:20px; box-shadow: 2px 2px 6px #aaaaaa;margin-left:200px;border-radius:20px;background-color:#FFFFFF">
+				        <img src="/img/4.png" style="float:left ;height:30px;width:30px;padding-right:10px;padding-top:3px;border-radius:50%;"> 
+				       <p  style="text-align:left;font-weight: lighter;font-family:points;">
+					     Strengthen your portfolio by working on interesting projects.
+						 <p>
+				  </div>
+			   </div>
+			   <div  id="right"  style="float: right">
+			       <div  id="points5" style="height:30px ;width:420px;padding:20px; box-shadow: 2px 2px 6px #aaaaaa;margin-left:26px ;margin-right:200px;border-radius:20px;background-color:#FFFFFF">
+				    <img src="/img/5.png" style="float:left ;height:30px;width:30px;padding-right:10px;padding-top:3px;border-radius:50%;"> 
+				        <p style="text-align:left;font-weight: lighter;font-family:points;">
+						     Local customer and marketing support through our dedicated service team.
+						</p>
+				  </div>
+				  <br>
+				  
+				  <div id="points6" style="height:30px ;width:420px;padding:20px; box-shadow: 2px 2px 6px #aaaaaa;margin-left:26px;margin-right:200px;border-radius:20px;background-color:#FFFFFF">
+				         <img src="/img/6.png" style="float:left ;height:30px;width:30px;padding-right:10px;padding-top:3px;border-radius:50%;"> 
+				        <p style="text-align:left;font-weight: lighter;font-family:points;">
+						    Payment fulfilled by Ghar Banao at every step of the way.
+						</p>
+				  </div>
+				  <br>
+				  
+				  <div   id="points7" style="height:30px ;width:420px;padding:20px; box-shadow: 2px 2px 6px #aaaaaa;margin-left:26px;margin-right:200px;border-radius:20px;background-color:#FFFFFF">
+				   <img src="/img/7.png" style="float:left ;height:30px;width:30px;padding-right:10px;padding-top:3px;border-radius:50%;"> 
+				       <p style="text-align:left;font-weight: lighter;font-family:points;">
+					       Customer Feedback and ratings on every project.
+					   <p>
+				  </div>
+				  <br>
+				  
+				  <div   id="points8" style="height:30px ;width:420px;padding:20px; box-shadow: 2px 2px 6px #aaaaaa;margin-left:26px;margin-right:200px;border-radius:20px;background-color:#FFFFFF">
+				   <img src="/img/8.png" style="float:left ;height:30px;width:30px;padding-right:10px;padding-top:3px;border-radius:50%;"> 
+				        <p  style="text-align:left;font-weight: lighter;font-family:points;"> You can showcase your work for free on your Designer&#39;s Profile Page.
+						</p>
+				  </div>
+			   </div>
+			     <br style="clear: left;" />
+				 <br>
+				 <br>
+				 <br>
+			 </div>
+			 <br>
+			 <br>
+			 <h1 id="joiningHeader">&nbsp;HOW TO JOIN &nbsp;</h1>
+			 <p id="tag">Join the journey and make yourself known </p>
+			  <div id="howitworks">
+			    <div>
+				  <br>
+				  <img src="/img/Apply_here.png" style="float:left ;height:90px;width:90px;border-radius:50%;">
+				   <br style="clear: left;" />
+				  <br>
+				  <h3 id="applyhere">APPLY  &nbsp;HERE</h3>
+				   <p class="how">
+				     Designers from acorss India willing to be a part of Ghar Banao have to fill a short online form.
+				  </p>
+				</div>
+				<div>
+				<br>
+				  <img src="/img/Get_verified.png" style="float:left ;height:90px;width:90px;border-radius:50%;">
+				  <br style="clear: left;" />
+				  <br>
+				  <h3 id="applyhere1">GET &nbsp; VERIFIED</h3>
+				  <p class="how">
+				   Since we are conscious about quality, designers portfolio and other documents will be verified before proceeding.
+				  </p>
+				</div>
+				  
+				<div>
+				 <br>
+				<img src="/img/Start_Working.png" style="float:left ;height:90px;width:90px;border-radius:50%;">
+				<br style="clear: left;" />
+				  <br>
+				  <h3 id="applyhere2">START &nbsp;WORKING</h3>
+				   <p class="how">
+				     Build your own profile, upload portfolio and start recieving orders from our clients.
+				  </p>
+				</div>
+			  </div>
+			  <div id ="CopyCat">
+			  <br style="clear: left;" />
+			  <img  id="CopyCatImg" src="/img/designerFooter.png" style="float:left ;height:220px;width:1366px;box-shadow: 4px 3px 7px #aaaaaa;">
+			  
+			  <button type="button">APPLY NOW</button>
+			  </div>
+			  <br>
+			  <br><br>
+			  <br>
+			  <br>
+			  <br>
+			  <div id="designerCollection">
+			  </div>
+			  
+			  
+			
+  `
+})
+export class AppComponent { }
